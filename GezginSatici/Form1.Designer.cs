8c439b3232c1c62b4647f3d256d56ad3ad20569e
zeyniep <75210764+zeyniep@users.noreply.github.com>
2022-06-20 18:04:23 +0300

    en son

--- GezginSatici/Form1.Designer.cs
+++ GezginSatici/Form1.Designer.cs
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.listView1 = new System.Windows.Forms.ListView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.listView2 = new System.Windows.Forms.ListView();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(10, 528);
+            this.button1.Location = new System.Drawing.Point(12, 493);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(194, 85);
@@ -51,19 +51,21 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::GezginSatici.Properties.Resources.istanbul_haritasi;
-            this.pictureBox1.Location = new System.Drawing.Point(232, 33);
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(241, 77);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(854, 822);
+            this.pictureBox1.Size = new System.Drawing.Size(1077, 501);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(10, 285);
+            this.listView1.Location = new System.Drawing.Point(12, 209);
             this.listView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(194, 235);
@@ -73,6 +75,7 @@
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
             this.textBox1.Location = new System.Drawing.Point(44, 92);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(125, 27);
@@ -100,30 +103,22 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // listView2
-            // 
-            this.listView2.Location = new System.Drawing.Point(1092, 157);
-            this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(445, 569);
-            this.listView2.TabIndex = 7;
-            this.listView2.UseCompatibleStateImageBehavior = false;
-            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1549, 786);
-            this.Controls.Add(this.listView2);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1349, 646);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Gezgin satıcı";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -138,6 +133,5 @@
         private TextBox textBox1;
         private Label label1;
         private Button button2;
-        private ListView listView2;
     }
 }
--- GezginSatici/Form1.cs
+++ GezginSatici/Form1.cs
@@ -1,10 +1,17 @@
-﻿namespace GezginSatici
+﻿using System.Drawing.Drawing2D;
+
+namespace GezginSatici
 {
     public partial class Form1 : Form
     {
+        //// this tracks the transformation applied to the PictureBox's Graphics
+        //private Matrix transform = new Matrix();
+        //public static float s_dScrollValue = 1.01f; // zoom factor
         public List<Location> location = new List<Location>();
         public List<Location> location2 = new List<Location>();
         public List<int> uzaklık = new List<int>();
+
+        float zoomratio = 1.059f;
         public int count = 0;
         public int s;
         int sayac = -1;
@@ -28,6 +35,7 @@
             if (count == 0)
             {
                 namelabel.Text = "◼";
+                
                 namelabel.ForeColor = System.Drawing.Color.Yellow;
 
             }
@@ -42,6 +50,70 @@
             lines();
             
         }
+        //private void pictureBox1_Canvas_Paint(object sender, PaintEventArgs e)
+        //{
+        //    Graphics g = e.Graphics;
+        //    g.Transform = transform;
+        //}
+        //protected override void OnMouseWheel(MouseEventArgs mea)
+        //{
+        //    pictureBox1.Focus();
+        //    if (pictureBox1.Focused == true && mea.Delta != 0)
+        //    {
+        //        ZoomScroll(mea.Location, mea.Delta > 0);
+        //    }
+        //}
+        //private void ZoomScroll(Point location, bool zoomIn)
+        //{
+        //    // make zoom-point (cursor location) our origin
+        //    transform.Translate(-location.X, -location.Y);
+
+        //    // perform zoom (at origin)
+        //    if (zoomIn)
+        //        transform.Scale(s_dScrollValue, s_dScrollValue);
+        //    else
+        //        transform.Scale(1 / s_dScrollValue, 1 / s_dScrollValue);
+
+        //    // translate origin back to cursor
+        //    transform.Translate(location.X, location.Y);
+
+        //    pictureBox1.Invalidate();
+        //}
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            if (e.Delta != 0)
+            {
+                if (e.Delta <= 0)
+                {
+                    //set minimum size to zoom
+                    if (pictureBox1.Width < 50)
+                        // lbl_Zoom.Text = pictureBox1.Image.Size; 
+                        return;
+                }
+                else
+                {
+                    //set maximum size to zoom
+                    if (pictureBox1.Width > 10000000)
+                        return;
+                }
+                pictureBox1.Width += Convert.ToInt32(pictureBox1.Width * e.Delta / 1000);
+                pictureBox1.Height += Convert.ToInt32(pictureBox1.Height * e.Delta / 1000);
+                //pictureBox1.Width = (int)(pictureBox1.Width * zoomratio);
+                //pictureBox1.Height = (int)(pictureBox1.Height * zoomratio);
+                pictureBox1.Top = (int)(e.Y - zoomratio * (e.Y - pictureBox1.Top));
+                pictureBox1.Left = (int)(e.X - zoomratio * (e.X - pictureBox1.Left));
+                
+            }
+
+        }
+
+
+
+
+
+
+
+
         void lines()
         {
             var itemCount = 0;
@@ -63,53 +135,115 @@
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //int maliyet1 = 0;
+            //foreach (var item in location)
+            //{
+            //    for (var j=1;j< location.Count; j++)
+            //    {
+            //        maliyet1 = ((Math.Abs(item.A - location[j].A) ^ 2 + Math.Abs(item.B - location[j].B) ^ 2) ^ 1 / 2);
+            //        uzaklık.Add(maliyet1);
+            //    }
+            //    uzaklık.Sort();
+            //    for (var k = 1; k < location.Count; k++)
+            //    {
+            //        maliyet1 = ((Math.Abs(item.A - location[k].A) ^ 2 + Math.Abs(item.B - location[k].B) ^ 2) ^ 1 / 2);
+            //        if (maliyet1== uzaklık[0])
+            //        {
+            //            Graphics myCanvas = pictureBox1.CreateGraphics();
+            //            Pen graph = new Pen(Color.Blue, 4);
+            //            myCanvas.DrawLine(graph, new Point(item.A, location[k].B), new Point(item.A, location[k].B));
+            //        }
+            //    }
+
+            //}
+
+
+
+
+
+
+
+
             var sabit = 0;
+            int maliyet1 = 0;
+            int maliyet2 = 0;
+
+
+
+
             foreach (var item in location)
             {
                 sayac++;
-                location2.Remove(item);
-                foreach (var item2 in location2)
+                
+                for (var i = 1; i < location.Count; i++)
                 {
-                   
+                    //var eski = sabit;
+                    if (item.A == location[i].A & item.B == location[i].B)
+                    {
+                        continue;
+                    }
 
+                    maliyet1 = ((Math.Abs(item.A - location[i].A) ^ 2
+                        + Math.Abs(item.B - location[i].B) ^ 2) ^ 1 / 2);
 
-                        var eski = sabit;
-                        var maliyet1 = 0;
-                        maliyet1 = ((Math.Abs(item.A - item2.A)^2 + Math.Abs(item.B- item2.B)^2) ^ 1 / 2);
+                  
+
+                    uzaklık.Add(maliyet1);
+                    Console.WriteLine(uzaklık);
+
+                    string maliyet = maliyet1.ToString();
+                    //listView2.Items.Add(maliyet+" "+item.A+ " "+ location[i].A);
+                    
+                    //        //if (maliyet1 < eski)
+                    //        //{
+                    //        //    Graphics myCanvas = pictureBox1.CreateGraphics();
+                    //        //    Pen graph = new Pen(Color.Blue, 4);
+                    //        //    myCanvas.DrawLine(graph, new Point(item.A, item.B), new Point(location[i].A, location[i].B));
+
+                    //        //}
+                    //        //else
+                    //        //{
+                    //        //    Graphics myCanvas = pictureBox1.CreateGraphics();
+                    //        //    Pen graph = new Pen(Color.Red, 4);
+                    //        //    myCanvas.DrawLine(graph, new Point(item.A, item.B), new Point(item2.A, item2.B));
+                    //        //}
+
+                    //        //location2.Remove(item);
+                       }
+                
+                    
+            for (int i = 1; i < location.Count; i++)
+            {
+                    if (item.A == location[i].A & item.B == location[i].B)
+                    {
+                        continue;
+                    }
+
+                    maliyet2 = ((Math.Abs(item.A - location[i].A) ^ 2 + Math.Abs(item.B - location[i].B) ^ 2) ^ 1 / 2);
+                Console.WriteLine(maliyet2);
+                    Console.WriteLine(uzaklık.Min());
+                    Console.WriteLine(uzaklık.Max());
+                    if (maliyet2 == uzaklık.Min())
+                {
+                    Graphics myCanvas = pictureBox1.CreateGraphics();
+                    Pen graph = new Pen(Color.Blue, 4);
+                    myCanvas.DrawLine(graph, new Point(item.A, item.B), new Point(location[i].A, location[i].B));
+
+                        uzaklık.Clear();
                         
-                        if (eski == 0)
-                        {
-                            eski = maliyet1;
-                        }
-                        sabit = maliyet1;
-
-                        //uzaklık.Add(maliyet1);
-                        //uzaklık.Sort();
-
-                        string maliyet = sayac.ToString();
-                        listView2.Items.Add(maliyet);
-                        
-
-                        if (maliyet1 < eski)
-                        {
-                            Graphics myCanvas = pictureBox1.CreateGraphics();
-                            Pen graph = new Pen(Color.Blue, 4);
-                            myCanvas.DrawLine(graph, new Point(item.A, item.B), new Point(item2.A, item2.B));
-                                                        
-                        }
-                        else
-                        {
-                            Graphics myCanvas = pictureBox1.CreateGraphics();
-                            Pen graph = new Pen(Color.Red, 4);
-                            myCanvas.DrawLine(graph, new Point(item.A, item.B), new Point(item2.A, item2.B));
-                        }
-                       
+                        Console.WriteLine(location);
+                     break;
 
                     }
-                    
-                }
+                   
             }
+               
+
+        }
+
+    }
         
+
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -127,6 +261,12 @@
         private void listView2_SelectedIndexChanged(object sender, EventArgs e)
         {
             
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+           
         }
     }
 }
